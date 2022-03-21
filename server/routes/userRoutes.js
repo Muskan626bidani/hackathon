ca75d7ca9eab7e077f53804d5383903ad9c69b11
Shreya <kundushreya312@gmail.com>
2022-03-22 05:00:28 +0530

    checkAdmin middleware, generateLoginId utility, update pwd route added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const UserController = require('../controllers/userController');
 const auth = require("../middleware/auth");
+const checkAdmin = require("../middleware/checkAdmin")
 
-router.post('/newUser', UserController.createUser);
-router.post('./userDetails', auth, UserController.getDetails);
+router.post('/newUser/:id',auth, checkAdmin, UserController.createUser);
+router.get('./userDetails', auth, UserController.getDetails);
 router.post("/login", UserController.loginUser);
 router.post("/resetPwd", UserController.resetPwd);
 router.post("/submit", auth, UserController.submitForm);
-
+router.post("/updateDetails",auth, UserController.updateDetails)
 
 module.exports=router;
