a0f65943a8c87284c529f335d965a7395e51117e
Lakshay-prog2503 <79397379+Lakshay-prog2503@users.noreply.github.com>
2022-03-21 19:24:14 +0530

    Connected Frontend to backend using userContext

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,28 +2,81 @@ const bcrypt = require("bcryptjs");
 const generateToken = require("../utils/generateToken");
 const User = require("../models/user");
 
+const createUser = async (req, res) => {
+
+	let success = false;
+	try {
+		let user = await User.findOne({
+			loginID: req.body.loginID,
+		});
+		if (user) {
+			success = false;
+			return res.status(400).json({
+				success,
+				error: "loginID already Exists"
+			});
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const secPass = await bcrypt.hash(req.body.password, salt);
+
+		user = await User.create({
+			name: req.body.name,
+			phoneNumber: req.body.phoneNumber,
+			email: req.body.email,
+			loginID: req.body.loginID,
+			password: secPass,
+			location: req.body.location
+		});
+
+		const data = {
+			user: {
+				id: user.id
+			}
+		}
+
+		const authToken = generateToken(data);
+
+		success = true
+		res.json({ success, authToken })
+	} catch (error) {
+		console.log(error.message)
+		res.status(500).send("Internal Server Error");
+	}
+}
+
 const loginUser = async (req, res) => {
-	const { email, password } = req.body;
+	const { loginID, password } = req.body;
 
 	try {
-		if (!(email && password)) {
-			res.status(400).send("Missing fields");
+		let success = false;
+		if (!(loginID && password)) {
+			success = false;
+			res.status(400).send({
+				success,
+				error: "Missing fields"
+			});
 		}
 
 		const existingUser = await User.findOne({
-			email: email.toLowerCase(),
+			loginID: loginID
 		});
 		if (!existingUser)
-			return res.status(404).json({ message: "User doesn't exist" });
+			return res.status(404).json({
+				success,
+				error: "User doesn't exist"
+			});
 
 		const isPasswordValid = await bcrypt.compare(
 			password,
 			existingUser.password
 		);
 		if (!isPasswordValid)
-			return res.status(401).json({ message: "Invalid password" });
-		
-          res.status(200).json({
+			return res.status(401).json({ success, error: "Invalid password" });
+
+		success = true;
+		res.status(200).json({
+			success,
 			user: existingUser,
 			token: generateToken(existingUser._id),
 		});
@@ -32,7 +85,7 @@ const loginUser = async (req, res) => {
 	}
 };
 
-const resetPwd = async (req, res) => { 
+const resetPwd = async (req, res) => {
 	const { mail } = req.body;
 	try {
 		if (!mail) {
@@ -46,7 +99,7 @@ const resetPwd = async (req, res) => {
 			return res.status(404).json({ message: "User doesn't exist" });
 
 		const token = generateToken(existingUser._id);
-		const text = `Hi ${existingUser.username},\n\nYou recently requested to reset your password for your account.\n\nPlease click on the following link, or paste this into your browser to complete the process:\n\nhttp://localhost:3000/reset/${token}\n\nIf you did not request this, please ignore this email and your password will remain unchanged.\n\nThanks,\n\nTeam`;
+		const text = `Hi ${existingUser.name},\n\nYou recently requested to reset your password for your account.\n\nPlease click on the following link, or paste this into your browser to complete the process:\n\nhttp://localhost:3000/reset/${token}\n\nIf you did not request this, please ignore this email and your password will remain unchanged.\n\nThanks,\n\nTeam`;
 		const subject = "Password Reset Request";
 		const email = existingUser.email;
 		const sendEmail = require("../utils/sendEmails");
@@ -59,4 +112,16 @@ const resetPwd = async (req, res) => {
 		res.status(500).json({ message: "Something went wrong" });
 	}
 }
-module.exports={loginUser,resetPwd};
+
+const getDetails = async (req, res) => {
+	try {
+		const userID = req.user.id;
+		const user = await User.findById(userID);
+		select("-password");
+		res.send(user);
+	} catch (error) {
+		console.log(error.message);
+		res.status(500).send("Internal Server Error");
+	}
+}
+module.exports = { createUser, loginUser, resetPwd, getDetails };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const UserController = require('../controllers/userController');
 const auth = require("../middleware/auth");
 
+router.post('/newUser', UserController.createUser);
+router.post('./userDetails', auth, UserController.getDetails);
 router.post("/login", UserController.loginUser);
-router.post("/resetPwd", UserController.resetPwd);
+router.post("/resetPwd", auth, UserController.resetPwd);
 
 module.exports=router;
