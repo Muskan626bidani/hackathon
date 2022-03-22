9c4526481dba684f39c5b32699a3add01362fb0d
ShubhamDhanda <shubhamdhanda944@gmail.com>
2022-03-22 12:19:23 +0530

    new

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import InnerPage from './components/InnerPage';
 import Login from './components/Login';
 import UserState from './context/users/userState';
 import Loginpage from './components/Loginpage';
+import Admin from './components/Admin';
+import Form from './components/Form';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
-   <Router>
-    <UserState>    
-     <Routes>
-        <Route path='' element={<Login />} />
-        <Route path='/login' element={<Loginpage />} />
-      </Routes>
-    </UserState>
-    </Router>
+  //  <Router>
+  //   <UserState>    
+  //    <Routes>
+  //       <Route path='' element={<Login />} />
+  //       <Route path='/Admin' element={<Admin />} />
+  //     </Routes>
+  //   </UserState>
+  //   </Router>
+    <Form />
     )
   }
 
