8ab4abde18ae131cec5f3803ea3482af2ea67777
Muskan Bidani <80620237+Muskan626bidani@users.noreply.github.com>
2022-03-22 20:46:56 +0530

    new user form

---
+++ src/components/NewUserReq.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './NewUserReq.css'
+export default function 
+() {
+  return (
+    <div className='newuser-form'>
+            <h1>New User Request</h1>
+        <form method="post">
+        <div className="change">
+            <input required type="text" />
+            <span></span>
+            <label>Name</label>
+        </div>
+        <div className="change">
+            <input required type='email' />
+            <span></span>
+            <label>Email</label>
+        </div>
+        <div className="change">
+            <input required type="number"/>
+            <span></span>
+            <label>Phone Number</label>
+        </div>
+        <div className="change">
+            <input required type="password" />
+            <span></span>
+            <label>Password</label>
+        </div>
+        <div className="change">
+            <input required type="text" />
+            <span></span>
+            <label>Location</label>
+        </div>
+            <button className="submit-button" >Submit</button>
+        </form>
+    </div>
+  )
+}
